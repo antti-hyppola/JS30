7299f547627a239904830d0aff2b68108116516c
antti <antti.hyppola@hotmail.com>
2020-11-18 12:03:38 +0000

    day 10

---
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -0,0 +1,20 @@
+const checkboxes = document.querySelectorAll('.inbox [type=checkbox]')
+
+let lastChecked;
+let inBetween = false;
+
+const handleCheck = function (e) {
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
